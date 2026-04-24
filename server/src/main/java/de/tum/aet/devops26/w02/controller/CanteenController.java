582780ac2a1fae99af5b81ee064e4c27a137e297
Yi Rui Cui <yirui.cui@tum.de>
2026-04-24 13:11:09 +0200

    implemented view daily menu functionality

--- server/src/main/java/de/tum/aet/devops26/w02/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops26/w02/controller/CanteenController.java
@@ -1,14 +1,15 @@
 package de.tum.aet.devops26.w02.controller;
 
-import de.tum.aet.devops26.w02.model.Dish;
-import de.tum.aet.devops26.w02.service.CanteenService;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import de.tum.aet.devops26.w02.model.Dish;
+import de.tum.aet.devops26.w02.service.CanteenService;
 
 @CrossOrigin
 @RestController
@@ -22,12 +23,13 @@ public class CanteenController {
 
     /**
      * Get today's meals for a specific canteen
+     * 
      * @param canteenName the ID of the canteen (e.g., "mensa-garching")
      * @return list of dishes available today at the specified canteen
      */
     @GetMapping("/{canteenName}/today")
     public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
-        // TODO: Implement the logic to fetch today's meals for the specified canteen using the CanteenService
-        return ResponseEntity.ok(List.of());
+        List<Dish> todayMeals = canteenService.getTodayMeals(canteenName);
+        return ResponseEntity.ok(todayMeals);
     }
 }
